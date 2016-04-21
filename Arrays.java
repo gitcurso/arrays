2b321c15668012c99e133b3c595cab95045eb5ee
Marcos Cuenca <flixpam@gmail.com>
2016-04-21 13:26:34 +0200

    añadido por marcos

--- Arrays.java
+++ Arrays.java
@@ -2,5 +2,6 @@ public class Arrays {
   public static void main(String[] args) {
     int [] vector = { 1, 2, 3, 4, 5 }; 
     System.out.println(vector[0]);
+    System.out.println("Markus");
   }
 }
