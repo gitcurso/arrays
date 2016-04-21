e36ffc21178ab566c655cef137f993d7b4520e27
Marta Pardo <ynedisanima@gmail.com>
2016-04-21 13:26:16 +0200

    Modificado por ynedis

--- Arrays.java
+++ Arrays.java
@@ -2,5 +2,7 @@ public class Arrays {
   public static void main(String[] args) {
     int [] vector = { 1, 2, 3, 4, 5 }; 
     System.out.println(vector[0]);
+
+    System.out.println("Modificado por ynedis");
   }
 }
